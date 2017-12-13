a311a1e349525b775d12ac11c17dcf67d4808dbd
Nikhila <gnikhila16@gmail.com>
2017-12-13 14:43:07 -0500

    connected to an restful api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { PostjsonComponent } from './postjson/postjson.component';
+import {GetjsondataService} from './getjsondata.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    AboutusComponent,
+    PageNotFoundComponent,
+    PostjsonComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AppRoutingModule
+  //   RouterModule.forRoot([
+  //    {
+  //     path : 'products',
+  //     component : ProductComponent  
+  //    }
+  // ])
   ],
-  providers: [],
+  providers: [GetjsondataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
